8651a94e5b68cb90dfc9614fcb30fe3273006506
Fabio S. <fabio_sc12@yahoo.com.br>
2024-09-24 21:42:27 -0300

    Adicionando o resultado, pode ter modificação

---
+++ script.js
@@ -0,0 +1,12 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+}
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+function evaluateResult() {
+    var result = document.getElementById('result').value;
+    var calculatedResult = eval(result);
+
+    document.getElementById('result').value = calculatedResult;
+}
