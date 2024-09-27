d61557cc916147998fd32d004ea5115fdded4581
Fabio S. <fabio_sc12@yahoo.com.br>
2024-09-27 20:38:16 -0300

    adicionando historico para a calculadora

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
+let history = [];
+
 function appendToResult(value) {
     document.getElementById('result').value += value;
 }
+
 function clearResult() {
     document.getElementById('result').value = '';
 }
-function evaluateResult() {
-    var result = document.getElementById('result').value;
-    var calculatedResult = eval(result);
 
-    document.getElementById('result').value = calculatedResult;
+function evaluateResult() {
+    const resultField = document.getElementById('result');
+    const result = resultField.value;
+    try {
+        const calculatedResult = eval(result);
+        const dateTime = new Date().toLocaleString();
+        resultField.value = calculatedResult;
+        addToHistory(calculatedResult, `Gerado em ${dateTime}`);
+    } catch (error) {
+        resultField.value = 'Erro';
+    }
+}
+
+function addToHistory(calculatedResult, dataTime){
+    history.push({calculatedResult, dataTime});
+    updateHistoryTable();
+}
+
+function updateHistoryTable(){
+    const tableBody=document.getElementById('historyTable').getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = '';
+    
+    history.forEach(({dataTime, calculatedResult}) => {
+        const row = tableBody.insertRow();
+        const cellResult = row.insertCell(0);
+        const cellData = row.insertCell(1);
+        cellResult.textContent = calculatedResult;
+        cellData.textContent = dataTime;
+
+        cellResult.onclick = () => {
+            document.getElementById('result').value = calculatedResult;
+        };
+    });
 }
